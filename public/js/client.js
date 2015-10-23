124955efb90293d702177b46a97f0ba66687a3d9
Randomguy_ <anon.dev.acct@gmail.com>
2015-10-23 09:06:19 -0500

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -719,7 +719,7 @@ $(function() {
             if (message.hat != 'nohat' && message.type == 'chat-message') {
                 $('<span class="hat ' + message.hat + '" style="background:url(\'/css/img/hats/'+message.hat+'.png\') no-repeat center;background-size: 30px 30px;"></span>').appendTo(content);
             }
-            var flair = (parsedFlair || message.nick) + ':';
+            var flair = $.parseHTML((parsedFlair || message.nick) + ':');
             $('<span class="nick"></span>').html(flair).appendTo(content);
         }
         if (message.message) {
