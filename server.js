2066b1208a6db6ebd722e6c150591ffc4dc387fb
Anonymous Coder <anon.dev.acct@gmail.com>
2015-11-17 12:51:23 -0600

    Update server.js
    jsdom doesn't seem to work with npm on linux; also it's not being used.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var dao = require('./dao');
 var throttle = require('./throttle');
 var request = require('request');
 var crypto = require('crypto');
+//var jsdom = require('jsdom');
 
 var _ = require('underscore');
 var $ = require('jquery-deferred');
